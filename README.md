b05ded4ed7c2ceaf0b8c537716e08c74572236c2
JCharante <git@jcharante.com>
2017-10-03 18:27:25 -0400

    initial working version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # quit-reddit
 Overwrite your comment history with a random line from Tiel la Mondo Iras
+
+# Environmental Variables
+
+```bash
+$ export client_id=REDDIT_SCRIPT_CLIENT_ID
+$ export client_sercet=REDDIT_SCRIPT_CLIENT_SECRET
+$ export password=YOUR_REDDIT_PASSWORD
+$ export user_agent="User Agent"
+$ export username=YOUR_REDDIT_USERNAME
+```
+
+# Running
+
+```bash
+$ python3.6 run.py
+```
---
+++ run.py
@@ -0,0 +1,73 @@
+# Python 3.6.1
+import praw
+import os
+import random
+
+
+reddit = praw.Reddit(client_id=os.environ['client_id'],
+					 client_secret=os.environ['client_secret'],
+					 password=os.environ['password'],
+					 user_agent=os.environ['user_agent'],
+					 username=os.environ['username'])
+
+lyrics = """Tiel la mondo iras, tiel la mondo iras.
+Tiel la mondo iras, tiel la mondo.
+Tiel la mondo iras, tiel la mondo iras.
+Tiel la mondo iras, tiel la mond'.
+
+Lundo, merkredo, sabato, mardo, ĵaŭdo kaj dimanĉo
+jen milito, jen la paco, jen infano kun malsato.
+La misiloj preskaŭ falas, la kolomboj malkonsentas,
+estas tiel, estas tiel.
+
+Jen virino kiu ne sidas, ĉar laboro ĉiam estas,
+kaj la patro kiu ne alvenas, ĉar la poŝo estas malplena.
+Tiom da manoj kiuj konstruas, kaj la aliaj kiuj detruas,
+estas tiel, estas tiel.
+
+Tiel la mondo iras, tiel la mondo iras.
+Tiel la mondo iras, tiel la mondo.
+Tiel la mondo iras, tiel la mondo iras.
+Tiel la mondo iras, tiel la mond'.
+
+Dekses horoj kiuj sonoras kaj ok horoj kiuj silentas
+Multaj homoj kiuj rapidas, jam la alia tago venas.
+Ni profitu la momenton, ĉar la vivo ne atendas,
+estas tiel, estas tiel
+
+Lundo, merkredo, sabato, mardo, jaŭdo kaj dimanĉo
+jen milito, jen la paco, jen infano kun malsato.
+La misiloj preskaŭ falas, la kolomboj malkonsentas,
+estas tiel, estas tiel.
+
+Tiel la mondo iras, tiel la mondo iras.
+Tiel la mondo iras, tiel la mondo.
+Tiel la mondo iras, tiel la mondo iras.
+Tiel la mondo iras, tiel la mond'.
+
+Iras por mi, iras por vi,
+iras por ŝi, iras por li, tiel la mondo
+Iras por mi, iras por vi,
+iras por ŝi, iras por li, tiel la mondo
+
+Tiel la mondo iras, tiel la mondo iras.
+Tiel la mondo iras, tiel la mondo.
+Tiel la mondo iras, tiel la mondo iras.
+Tiel la mondo iras, tiel la mond-ooo iiirrrr-aaass""".split('\n')
+lyrics = [line for line in lyrics if len(line) > 0]
+
+print(f"Logged in as {reddit.user.me()}")
+
+safe_subreddits = []
+
+for subreddit in reddit.user.moderator_subreddits():
+	safe_subreddits.append(subreddit.display_name)
+
+print(f"Will not deleted comments in: {safe_subreddits}")
+
+for comment in reddit.user.me().comments.new(limit=None):
+	if comment.subreddit in safe_subreddits:
+		continue
+	random_lyric = random.SystemRandom().choice(lyrics)
+	print(f"Changing ({comment}) in {comment.subreddit}:\nOriginally:\n{comment.body}\nAnd is now:\n{random_lyric}\n")
+	comment.edit(random_lyric)
